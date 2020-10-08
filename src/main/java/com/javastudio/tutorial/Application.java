cd29cc591cc59dcdff45a194db20de16580a0bf2
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-10-08 12:45:23 +0330

    maven executable jar

--- src/main/java/com/javastudio/tutorial/Application.java
+++ src/main/java/com/javastudio/tutorial/Application.java
@@ -1,7 +1,12 @@
 package com.javastudio.tutorial;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LOGGER.info("Hello world!");
     }
 }
